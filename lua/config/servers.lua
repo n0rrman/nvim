aeffcdb11d39294c64c8b033d56808d7aefd3d91
n0rrman <mail@henriknorrman.com>
2024-12-30 20:27:32 +0000

    mason servers removed from mini

--- lua/config/servers.lua
+++ lua/config/servers.lua
@@ -13,10 +13,6 @@ return {
 		yaml = { "prettierd", "prettier", stop_after_first = true },
 		markdown = { "prettierd", "prettier", stop_after_first = true },
 		graphql = { "prettierd", "prettier", stop_after_first = true },
-		lua = { "stylua" },
-		python = { "isort", "black" },
-		rust = { "rustfmt" },
-		go = { "goimports" },
 		["*"] = { "codespell" },
 	},
 
@@ -26,22 +22,8 @@ return {
 			-- General --
 			"codespell",
 			-- Web --
-			"eslint_d",
 			"prettier",
 			"prettierd",
-			"biome",
-			"quick-lint-js",
-			"prettier",
-			-- Lua --
-			"stylua",
-			-- Python --
-			"isort",
-			"black",
-			"flake8",
-			-- Rust --
-			"rustfmt",
-			-- Go --
-			"goimports",
 		},
 		ignore_install = {
 			"janet",
@@ -52,36 +34,7 @@ return {
 	},
 	-- LSP --
 	lsp = {
-		ensure_installed = {
-			-- General --
-			"harper_ls",
-            "emmet_ls",
-			-- Lua --
-			"lua_ls",
-			-- Angular --
-			"angularls",
-			-- Typescript --
-			"ts_ls",
-			"biome",
-			"denols",
-			"eslint",
-			-- CSS -
-			"tailwindcss",
-			"cssls",
-			-- Go --
-			"gopls",
-			"templ",
-			"golangci_lint_ls",
-			-- Rust --
-			"rust_analyzer",
-			-- C/C++ --
-			"clangd",
-			-- HTML --
-			"html",
-			-- Python --
-			"pyright",
-			"pylsp",
-		},
+		ensure_installed = {},
 		ignore_install = {},
 	},
 }
